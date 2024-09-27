9065102cec0910cd7750836f33d2e5c24d2877cb
Igor Hasse Santiago <igor.hasse@gmail.com>
2024-09-27 20:59:50 -0300

    Dark mode, git-ignore updated

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -8,15 +8,23 @@ import { useUser, UserButton } from "@clerk/nextjs";
 function Header() {
   const { user, isSignedIn } = useUser();
   return (
-    <div className="p-5 flex justify-center items-center border shadow-sm">
-      <div>
-        {/* Logo */}
-        <span>FinanSmart</span>
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <div className="flex flex-row items-center">
+        <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
+        <span className=" font-bold text-xl">FinanSmart</span>
       </div>
-      <div>
-        <button> Button 1</button>
-        <button> Button 2</button>
-      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div>
+          <Link href="/dashboard">
+            <Button>Dashoard</Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button>Get started</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
